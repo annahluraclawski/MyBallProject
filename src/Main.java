e88f64f97f0b1af22c3389567b5701e1daa561b5
Annahlu <>
2023-01-24 17:22:08 +0000

    Added a print statement

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 public class Main{
     public static void main(String[] args){
+      System.out.println("Making an online replit");
         JFrame newFrame = new JFrame();
         newFrame.setSize(500, 500);
         DrawPanel newPanel = new DrawPanel();
