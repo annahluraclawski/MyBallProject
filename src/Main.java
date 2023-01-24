cd1abec62adbcf000e69a3a4157dcfbacea9cf1d
annahlu.raclawski24 <annahlu47@gmail.com>
2023-01-24 11:29:25 -0600

    Added another

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.awt.*;
 public class Main{
     public static void main(String[] args){
       System.out.println("Making an online replit");
+      System.out.println("Now Im on intelliJ");
         JFrame newFrame = new JFrame();
         newFrame.setSize(500, 500);
         DrawPanel newPanel = new DrawPanel();
